de2c0f555faee14a829804d0678aa883d15146b3
aftab alam <aftabdeveloper8368@gmail.com>
2023-11-21 18:20:07 +0530

    razorpay webhook integration in progress

--- controller/razorpay.controller.js
+++ controller/razorpay.controller.js
@@ -1,5 +1,5 @@
 import Razorpay from "@/modules/razorpay.modules"
-
+import crypto from "crypto"
 export const createOrder = async (req,res)=>{
     try
     {
@@ -10,6 +10,32 @@ export const createOrder = async (req,res)=>{
             receipt: "dot-"+Date.now()
           })
           res.status(200).json(x)
+    }
+    catch(err)
+    {
+        res.status(500).json(err)
+    }
+}
+
+export const verifyPayment = (req,res)=>{
+    try
+    {
+        const secret = 'aftab';
+        console.log(req);
+
+        const shasum = crypto.createHmac("sha256", secret);
+        shasum.update(JSON.stringify(req.body));
+        const digest = shasum.digest("hex");
+
+        console.log(digest, req.headers["x-razorpay-signature"]);
+
+        if (digest === req.headers["x-razorpay-signature"]) {
+            console.log("request is legit");
+            console.log(JSON.stringify(req.body, null, 4));
+        } else {
+            // ignore it
+        }
+        res.json({ status: "ok" });
     }
     catch(err)
     {
--- pages/test.js
+++ pages/test.js
@@ -14,17 +14,15 @@ const Test = ()=>{
         key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
         amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
         currency: "INR",
-        name: "Acme Corp",
+        name: "DOT PRODUCT",
         description: "React course payment",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png",
         order_id: id, //This is a sample Order ID. Pass the `id` obtained in the response of createOrder().
-        handler: function (response) {
-          alert(response.razorpay_payment_id);
-          alert(response.razorpay_order_id);
-          alert(response.razorpay_signature);
+        handler: async function (response) {
+          const x = await axios.post("/api/razorpay/payment",response)
         },
         prefill: {
-          name: "Piyush Garg",
+          name: "aftab",
           email: "youremail@example.com",
           contact: "9999999999",
         },
@@ -37,16 +35,6 @@ const Test = ()=>{
         
     }
     const rzp1 = new Razorpay(options);
-
-    rzp1.on("payment.failed", function (response) {
-      alert(response.error.code);
-      alert(response.error.description);
-      alert(response.error.source);
-      alert(response.error.step);
-      alert(response.error.reason);
-      alert(response.error.metadata.order_id);
-      alert(response.error.metadata.payment_id);
-    });
   
     rzp1.open();
 }
